46bb54241c1e4ffc46bbc3833d6fb9f368b03fc8
HAEUNP <superhaun@naver.com>
2025-04-02 10:01:25 +0900

    feat: crud 가능 추가

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Login from "./pages/Login";
 import AfterLogin from "./pages/AfterLogin";
+import OAuthCallback from "./pages/OAuthCallback";
 import WriteLetter from "./pages/WriteLetter";
-import OAuthCallback from "./pages/OAuthCallback"; // ✅ 이거 꼭 필요함
-import SelectPlanet from "./pages/SelectPlanet"; 
-import SolarSystem from "./pages/SolarSystem"; 
+import SelectPlanet from "./pages/SelectPlanet";
+import SolarSystem from "./pages/SolarSystem";
+import LoginFail from "./pages/LoginFail"; // 로그인 실패 페이지
 
 function App() {
-  console.log("App loaded"); 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/oauth/callback/kakao" element={<OAuthCallback />} /> {/* ✅ 이 라우트도 필요 */}
-        <Route path="/after-login" element={<AfterLogin />} />
-        <Route path="/write" element={<WriteLetter />} />
-        <Route path="/select-planet" element={<SelectPlanet />} />
-        <Route path="/solar-system" element={<SolarSystem />} />
-        <Route path="/write-letter" element={<WriteLetter />} />
-
+        <Route path="/" element={<Login />} />                  // 메인(로그인) 페이지
+        <Route path="/after-login" element={<AfterLogin />} /> // 로그인 후 화면
+        <Route path="/oauth2/redirect" element={<OAuthCallback />} /> // 백엔드에서 redirect되는 경로
+        <Route path="/write-letter" element={<WriteLetter />} />      // 편지 작성
+        <Route path="/select-planet" element={<SelectPlanet />} />    // 행성 선택
+        <Route path="/solar-system" element={<SolarSystem />} />      // 태양계 화면
+        <Route path="/login-fail" element={<LoginFail />} />          // 로그인 실패 시
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
